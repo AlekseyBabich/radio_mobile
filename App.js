972fe3aa1945bb61b9b97788fe840f5891234404
Леха Омск <babichalex1986@gmail.com>
2023-09-29 16:19:02 +0600

    create logo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
 import {HeaderLogo} from "./components/HeaderLogo";
 
 
