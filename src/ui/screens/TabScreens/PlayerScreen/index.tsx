91a4bad7b3acedfed1309fc4797807ee3b5903f8
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-10-02 21:45:25 +0600

    feat: add radio player

--- src/ui/screens/TabScreens/PlayerScreen/index.tsx
+++ src/ui/screens/TabScreens/PlayerScreen/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import {SafeAreaView, StyleSheet, Text} from 'react-native'
+import RadioPlayer, {
+    RadioPlayerEvents,
+} from 'react-native-radio-player';
 import { COLORS } from "../../../../assets/constant/color";
+import {useEffect} from "react";
 
 
 export const PlayerScreen = (): JSX.Element => {
+    useEffect(() => {
+        RadioPlayer.radioURLWithMetadataSeparator(
+            'https://streams.radio.co/se2fe41ec1/listen',
+            "-"
+        ).then();
+        return () => {
+            RadioPlayer.play().then();
+        }
+    }, [])
     return (
         <SafeAreaView style={styles.wrapper}>
             <Text style={styles.text}>Плеер</Text>
